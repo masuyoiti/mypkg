992a011319e02bd3d1d91768cb21cd6abd2ca6f8
yotuwo <yaiti0212@gmail.com>
2024-12-12 10:55:42 +0900

    第11回最後

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -2,16 +2,19 @@ import rclpy
 from rclpy.node import Node
 from person_msgs.msg import Person
 
+class Listener(Node):
+    def __init__(self):
+        super().__init__('listener')
+        self.sub = self.create_subscription(Person, 'person', self.cb, 10)
 
-rclpy.init()
-node = Node("listener")
+    def cb(self, msg):
+        self.get_logger().info(f'Listen: {msg.name}, Age: {msg.age}')
 
-
-def cb(msg):
-    global node
-    node.get_logger().info("Listen: %s" % msg)
-
-
-def main():
-    sub = node.create_subscription(Person,"person", cb, 10)
+def main(args=None):
+    rclpy.init(args=args)
+    node = Listener()
     rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,6 +1,20 @@
 import rclpy
 from rclpy.node import Node
-from person_msgs.msg import Person
+from person_msgs.msg import Int16
+
+
+class Talker(Node:)
+    def_init_(self):
+        super()._init_("Talker")
+        self.pub = self.create_publisher(Int16, "countup", 10)
+        self.creat_timer(0.5, self.cb)
+        self.n = 0
+
+
+def main():
+    rclpy.init()
+    node = Talker()
+    rclpy.spin(node)
 
 rclpy.init()
 node = Node("talker")
