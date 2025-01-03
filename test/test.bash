39e5a0e08c0daf0ec5495e5bf4785da215242a39
yotuwo <yaiti0212@gmail.com>
2025-01-03 16:50:50 +0900

    test change

--- test/test.bash
+++ test/test.bash
@@ -1,4 +1,8 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2024 Youichi Masuyama <yaiti0212@gmail.com>
+# SPDX-License-Identifier: BSD-3-Clause
+
+
 
 dir=~
 [ "$1" != "" ] && dir="$1"
@@ -8,7 +12,7 @@ colcon build
 source $dir/.bashrc
 
 # ノードを起動してログを確認
-timeout 10 ros2 run mypkg resource_monitor > /tmp/mypkg.log
+timeout 60 ros2 run mypkg resource_monitor > /tmp/mypkg.log
 
 # トピック名や出力内容を正確に確認
 cat /tmp/mypkg.log |
